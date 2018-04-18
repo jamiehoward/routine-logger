685db8a1a8775fc5a656a0fcf5fe13502c6c1519
Jamie Howard <jamiehowardco@gmail.com>
2018-04-18 12:04:47 -0400

    Added routine model and migration

---
+++ database/migrations/2018_04_18_155737_create_routines_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoutinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->enum(['numeric', 'interval']);
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routines');
+    }
+}

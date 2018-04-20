479deb15b00923c481bf406c1df0b5c94b5f4270
Jamie Howard <jamiehowardco@gmail.com>
2018-04-20 15:53:19 -0400

    Added splash image

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,6 +8,7 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name = "viewport" content = "initial-scale = 1.0, user-scalable = no">
     <link rel="apple-touch-icon" href="/img/icon.png">
+    <link rel="apple-touch-startup-image" href="/img/splash.png" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
